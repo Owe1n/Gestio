7ad10281d413f5b92291bf15aa79cbefdcfe3744
Enzo Rodrigues <enzo.rodrigues.dos.reis@gmail.com>
2021-05-22 15:33:17 +0200

    Hello World 2

--- Gestio/src/gestio/MainApp.java
+++ Gestio/src/gestio/MainApp.java
@@ -3,5 +3,6 @@ package gestio;
 public class MainApp {
 	 public static void main(String[] args) {
 	        System.out.println("Hello, World!"); 
+	        System.out.println("Hello, World!"); 
 	    }
 }
