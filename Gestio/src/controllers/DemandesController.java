95cf7c33336bf928349e3ffcedf0dfd18571b783
Enzo Rodrigues <enzo.rodrigues.dos.reis@gmail.com>
2021-05-24 22:39:59 +0200

    Début partie Demande Ajouts images + logos Ajout des fonts

---
+++ Gestio/src/controllers/DemandesController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DemandesController extends MainController {
+	
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		System.out.println("curent page : Login demandes ");
+		
+	}
+	
+}
