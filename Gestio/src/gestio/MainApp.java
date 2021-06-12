420e121d1a3f8635b38a2ac0f67eae60c5c2a26c
Enzo Rodrigues <enzo.rodrigues.dos.reis@gmail.com>
2021-06-12 18:00:55 +0200

    Gestio V1

--- Gestio/src/gestio/MainApp.java
+++ Gestio/src/gestio/MainApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -15,14 +16,14 @@ public class MainApp extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-
-		
 		
 		// TODO Auto-generated method stub
 		Parent loginPage = FXMLLoader.load(getClass().getResource("/vues/Main.fxml"));
 		
 		Scene scene = new Scene(loginPage);
 		
+		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/img/logoApp.png")));
+		primaryStage.setTitle("Gestio");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
